361ea76019bd961e493c29ca9f0892c241df50a3
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2019-12-25 19:37:45 +0300

    Searching added

--- src/main/java/com/example/SpringSite/GreetingController.java
+++ src/main/java/com/example/SpringSite/GreetingController.java
@@ -1,13 +1,19 @@
 package com.example.SpringSite;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepository messageRepository;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
@@ -16,9 +22,43 @@ public class GreetingController {
         model.put("name", name);
         return "greeting";
     }
+
     @GetMapping
-    public String main(Map<String ,Object> model){
-        model.put("some","Hello from localhost 8080");
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String addMessage(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("findByTag")
+    public String findMessageByTag(@RequestParam String find, Map<String, Object> model) {
+
+        List<Message> messages = messageRepository.findByTag(find);
+        model.put("messages",messages);
+        return "main";
+    }
+
+    @PostMapping("findByText")
+    public String findMessageByText(@RequestParam String find, Map<String, Object> model) {
+
+        List<Message> messages = messageRepository.findByText(find);
+        model.put("messages",messages);
+        return "main";
+    }
+    @PostMapping("reset")
+    public String reset(Map<String, Object> model){
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
         return "main";
     }
 
