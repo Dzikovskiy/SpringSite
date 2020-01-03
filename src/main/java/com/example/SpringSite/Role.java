d0d77ee8ba9795d765d45ce3fb777f833825292f
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-01-03 22:52:58 +0300

    Admin and user roles added

--- src/main/java/com/example/SpringSite/Role.java
+++ src/main/java/com/example/SpringSite/Role.java
@@ -2,9 +2,9 @@ package com.example.SpringSite;
 
 import org.springframework.security.core.GrantedAuthority;
 
-public enum Role  implements GrantedAuthority {
+public enum Role implements GrantedAuthority {
 
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
