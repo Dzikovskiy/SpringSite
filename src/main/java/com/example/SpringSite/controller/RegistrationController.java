4c72315e13047e636fd2e96ba466180ebb2f5b82
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-03-24 23:20:13 +0300

    Mail activation
    not ready

--- src/main/java/com/example/SpringSite/controller/RegistrationController.java
+++ src/main/java/com/example/SpringSite/controller/RegistrationController.java
@@ -1,21 +1,19 @@
 package com.example.SpringSite.controller;
 
-import com.example.SpringSite.Role;
 import com.example.SpringSite.User;
-import com.example.SpringSite.UserRepository;
+import com.example.SpringSite.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
     @GetMapping("/registration")
     public String registration(Model model){
         model.addAttribute("message", "");
@@ -23,15 +21,13 @@ public class RegistrationController {
     }
     @PostMapping("/registration")
     public String addUser(User user, Map<String,Object> model){
-       User userFormDb = userRepository.findByUsername(user.getUsername());
-       if(userFormDb!=null){
+
+       if(!userService.addUser(user)){
         model.put("message","User exists!");
         return "registration";
 
        }
-       user.setActive(true);
-       user.setRoles(Collections.singleton(Role.USER));
-       userRepository.save(user);
+
         return "redirect:/login";
 
     }
--- src/main/java/com/example/SpringSite/service/UserService.java
+++ src/main/java/com/example/SpringSite/service/UserService.java
@@ -1,19 +1,52 @@
 package com.example.SpringSite.service;
 
+import com.example.SpringSite.Role;
+import com.example.SpringSite.User;
 import com.example.SpringSite.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MailSender mailSender;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFormDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFormDb != null) {
+            return false;
+
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepository.save(user);
+
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format(
+                    "Hello, %s" + "\n" + "Welcome to our site: http://localhost:8080/activation/%s",
+                    user.getUsername(), user.getActivationCode()
+
+            );
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+        return false;
     }
 }
